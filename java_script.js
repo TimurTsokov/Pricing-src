43b5097397d881ad4fba7f8f1873744c4113dab4
TimurTsokov <tsokov2t1m5@gmail.com>
2018-07-05 14:41:45 +0300

    +js

---
+++ java_script.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+
+    // var popper = new Popper(referenceElement, onPopper, {
+    //     placement: 'right'
+    // });
+    // $('.btn').click(function () {
+    //     $.tooltip('show');
+    // });
+    $(function () {
+        // $('[data-toggle="tooltip"]').css({"color": "red;"});
+        $('[data-toggle="tooltip"]').tooltip()
+    });
+
+});
